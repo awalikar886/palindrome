cec95418fbc17640e9e7fb8b9dca0cdb7d744ed8
awalikar886 <awalikar886@gmail.com>
2026-01-16 09:22:51 +0530

    paluindrome

---
+++ p.py
@@ -0,0 +1,11 @@
+def palindrome(text):
+
+    result = (
+        f"text: {text}\n"
+    )
+
+    return result
+
+
+if __name__ == "__main__":
+    print(palindrome("madam"))
---
+++ test_p.py
@@ -0,0 +1,9 @@
+from p import palindrome
+ 
+def test_palindrome():
+     result = (
+        f"text: madam\n"
+    )
+     assert(palindrome("madam")) == result
+
+
